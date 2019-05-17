5e8ced4c6216ccf8b04167ecc5a98ad32255d71e
Nhinh Dao <nhinhdao.vn@gmail.com>
2019-05-17 02:01:00 -0400

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 require 'rest-client' 
 require 'json'
 
-resp = RestClient.get("https://api.openbrewerydb.org/breweries?by_state=new_york&sort=+name&per_page=40")
+resp = RestClient.get("https://api.openbrewerydb.org/breweries?by_state=new_york&per_page=40")
 response = JSON.parse(resp)
 response.each do |place|
   Place.create! do |pl|
@@ -25,7 +25,7 @@ response.each do |place|
     pl.country = place["country"]
     pl.longitude = place["longitude"] ? place["longitude"].to_f : -73.935242
     pl.latitude = place["latitude"] ? place["latitude"].to_f : 40.730610
-    pl.phone = place["phone"]
-    pl.website_url = place["website_url"]
+    pl.phone = place["phone"] ? place["phone"] : 'N/A'
+    pl.website_url = place["website_url"] ? place["website_url"] : 'N/A'
   end
 end
